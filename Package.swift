3b418d8658b4c67c6698dfbb1cb67b0b23849313
Swizzlr <git@swizzlr.co>
2017-01-10 12:24:28 +0000

    Include dependency on Titan Core.

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ let package = Package(
   name: "TitanBenchmark",
   dependencies: [
     .Package(url: "https://github.com/bermudadigitalstudio/titan-nest-adapter.git", majorVersion: 0, minor: 1),
+    .Package(url: "https://github.com/bermudadigitalstudio/titan-core.git", majorVersion: 0, minor: 1),
     .Package(url: "https://github.com/kylef/Curassow.git", majorVersion: 0, minor: 6)
   ]
 )
