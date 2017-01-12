ce1e26f1910e6cc378ee0780ec35355187c8dfc0
Swizzlr <git@swizzlr.co>
2017-01-12 14:17:32 +0000

    The great renaming.

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@ let package = Package(
   name: "TitanBenchmarks",
   dependencies: [
     // Titan Core
-    .Package(url: "https://github.com/bermudadigitalstudio/titan-core.git", majorVersion: 0, minor: 1),
+    .Package(url: "https://github.com/bermudadigitalstudio/TitanCore.git", majorVersion: 0, minor: 1),
     // Nest/Curassow
-    .Package(url: "https://github.com/bermudadigitalstudio/titan-nest-adapter.git", majorVersion: 0, minor: 1),
+    .Package(url: "https://github.com/bermudadigitalstudio/TitanNestAdapter.git", majorVersion: 0, minor: 1),
     .Package(url: "https://github.com/kylef/Curassow.git", majorVersion: 0, minor: 6),
     // Kitura
     .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 4),
-    .Package(url: "https://github.com/bermudadigitalstudio/titan-kitura-adapter.git", majorVersion: 0, minor: 1)
+    .Package(url: "https://github.com/bermudadigitalstudio/TitanKituraAdapter.git", majorVersion: 0, minor: 1)
   ]
 )
